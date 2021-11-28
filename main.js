9d40bf33dbcb15b2370c21165fb69745d1dd5818
Jasjeet393Singh <81762166+Jasjeet393Singh@users.noreply.github.com>
2021-11-28 22:21:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,24 @@
 function setup() {
-    canvas = createCanvas(600,600);
-    canvas.center()
+    video  = createCapture(VIDEO);
+    video.size(550,500);
+
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    background(225,80,47);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is Initialized')
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+    }
 }
